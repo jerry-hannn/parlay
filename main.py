f32d10e54a36efc28fa914e7d19ee9ce37c9cb3a
Jerry Han <han.gerald.2@gmail.com>
2025-12-16 19:55:32 -0600

    using NBA data loader

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Main file to run
+from src.data.nba_loader import fetch_nba_season_data
+
+def main():
+    # In the future, this year will be pulled from config/config.yaml
+    target_season = "2023-24"
+    
+    # 1. Ingestion
+    raw_file_path = fetch_nba_season_data(target_season)
+    
+    if raw_file_path:
+        print(f"Pipeline finished. Raw data ready at {raw_file_path}")
+
+if __name__ == "__main__":
+    main()
